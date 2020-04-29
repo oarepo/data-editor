9156fff302d032cceea1284716b9b4a01e575e8e
Ondrej Ruml <rumlo@vscht.cz>
2020-04-29 23:45:08 +0200

    add delete dialog component

---
+++ library/components/editors/DeleteDialogComponent.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+q-dialog(ref="dialog" @hide="onDialogHide")
+  q-card
+    q-card-section
+      q-item Chcete smazat tento záznam?
+    q-card-actions(align="right")
+      q-btn(color="primary" type="submit" label="OK" @click="onOKClick")
+      q-btn(color="primary" label="Cancel" @click="onCancelClick")
+</template>
+
+<script>
+export default {
+  name: 'delete-dialog',
+  methods: {
+    show () {
+      this.$refs.dialog.show()
+    },
+    hide () {
+      this.$refs.dialog.hide()
+    },
+    onDialogHide () {
+      this.$emit('hide')
+    },
+    async onOKClick () {
+      this.$emit('ok')
+      this.hide()
+    },
+    onCancelClick () {
+      this.hide()
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
