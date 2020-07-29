6348b5dc602e25484c6f95bae8c008ea4751afae
Ondrej Ruml <rumlo@vscht.cz>
2020-07-29 15:43:31 +0200

    edit build script

--- build.sh
+++ build.sh
@@ -11,5 +11,6 @@ $bili -c bili.config.js --plugins.vue --format esm ./index.js
 mv dist/index.esm.js dist/oarepo-data-editor.esm.js
 cat ../package.json | $json -e "delete this.scripts; delete this.devDependencies; delete this.dependencies" >dist/package.json
 cp ../README.md dist
+cp -r ../library/css dist
 mv dist ..
 
